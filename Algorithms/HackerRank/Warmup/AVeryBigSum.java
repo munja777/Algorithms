862c4f132cf5c835cf974853cefa130b5f5bf6ec
Artur Domijański <youremail@who.com>
2017-04-01 16:51:53 +0200

    second

---
+++ Algorithms/HackerRank/Warmup/AVeryBigSum.java
@@ -0,0 +1,16 @@
+package HackerRank.Warmup;
+
+import java.util.*;
+
+class AVeryBigSum {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int size = in.nextInt();
+        long sum = 0;
+        for (int i = 0; i < size; i++) {
+            sum += in.nextInt();
+        }
+        System.out.println(sum);
+    }
+}
