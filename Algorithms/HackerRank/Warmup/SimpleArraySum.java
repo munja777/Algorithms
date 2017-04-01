b1dd82b708a9ac99ed5a9f3e99ebdc8851732896
Artur Domijański <youremail@who.com>
2017-04-01 16:46:03 +0200

    second

---
+++ Algorithms/HackerRank/Warmup/SimpleArraySum.java
@@ -0,0 +1,17 @@
+package HackerRank.Warmup;
+
+import java.util.*;
+
+class SimpleArraySum {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+
+        int sum = 0;
+        for (int i=0; i<a; i++) {
+            sum += in.nextInt();
+        }
+        System.out.println(sum);
+    }
+}
