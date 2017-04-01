5f4a40b7312c4073d4c766be3d84c5691c553749
Artur Domijański <youremail@who.com>
2017-04-01 17:41:58 +0200

    first

---
+++ Algorithms/HackerRank/Warmup/PlusMinus.java
@@ -0,0 +1,23 @@
+package HackerRank.Warmup;
+
+import java.util.*;
+
+public class PlusMinus {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int size = in.nextInt();
+        int plus = 0;
+        int minus = 0;
+        int zero = 0;
+        for (int i = 0; i < size; i++) {
+            int a = in.nextInt();
+            if (a > 0) plus++;
+            else if (a < 0) minus ++;
+            else zero ++;
+        }
+        System.out.println((double)plus/size);
+        System.out.println((double)minus/size);
+        System.out.println((double)zero/size);
+    }
+}
